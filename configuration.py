7ad949f4e0f95f9710508e932fa414a6cf7e7353
William Alexander Yarian <williamalexanderyarian@gmail.com>
2025-03-18 20:55:04 -0600

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-a92cea8c-cb3a-4e64-afa6-8566b8f3399c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
